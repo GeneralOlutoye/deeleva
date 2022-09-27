193c982b1c8fd76eec28ab9094f15912a47a3658
Joel Olutoye <donronald27@gmail.com>
2022-09-27 12:18:59 +0100

    Update

---
+++ src/components/Business/section1.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Section1 = () => {
+  return (
+    <div>
+        <div className="section1-bg section1 flexBox">
+                <div className='section1-text-wrapper'>
+                    <h1 className="section1-h1">
+                        Own a bike or logistics <br /> business?
+                    </h1>
+                    <p className="section1-paragraph">
+                        Come partner with Deeleva and earn <strong>₦300,000</strong> monthly!
+                    </p>
+                    <button className='section1-button'> partner with us! </button>
+                </div>
+            </div>
+    </div>
+  )
+}
